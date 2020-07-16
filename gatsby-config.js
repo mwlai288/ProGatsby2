90c2ef38f037d95b7886d9351ba93117502464d2
mwlai288 <mwlai288@gmail.com>
2020-07-16 14:43:34 -0400

    add netflify headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,8 +29,6 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
   ],
 }
